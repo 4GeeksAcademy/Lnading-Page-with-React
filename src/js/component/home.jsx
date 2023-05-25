d4b9888cfe46894bc3cb43b3a7b133d91d3fed8c
Àlex Rofes Galobardes <129336104+RofinhoOo@users.noreply.github.com>
2023-05-25 16:46:15 +0000

    ejercicio final

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,40 +1,39 @@
 import React from "react";
-
 import Card from "./card/Card";
-
 import Jumbotron from "./jumbotron/Jumbotron";
-
 import Navbar from "./navbar/Navbar";
 
-
-
-//create your first component
 const Home = () => {
-	const Cards = {
+  const Cards = {
+    title: ["title1", "title2", "title3", "title4"],
+    description: ["description1", "description2", "description3", "description4"]
+  };
 
-		title: ["title1", "title2", "title3", "title4"],
-		description:["description1", "description2", "description3", "description4"]
-		
-	}
-	const Aux = [1, 2, 3, 4]
-	return (
-		<>
-		<Navbar/>
-		<Jumbotron title={"A Warm Welcome!"} description={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat."} bottonDescription={"Call to action!"}/>
-		{Cards.title.map((title, index) => (
-        <div className= "d-flex containt-fluid-start row">
-		<Card
-          key={index}
-          title={title}
-          description={Cards.description[index]}
-          bottonDescription={"bottonJumbotron"}
-        /></div>
-      ))}
-
-	  
-		
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <Jumbotron
+        title={"A Warm Welcome!"}
+        description={
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat."
+        }
+        bottonDescription={"Call to action!"}
+      />
+      <div className="d-flex container-fluid">
+        <div className="row">
+          {Cards.title.map((title, index) => (
+            <div key={index} className="col">
+              <Card
+                title={title}
+                description={Cards.description[index]}
+                bottonDescription={"bottonJumbotron"}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
